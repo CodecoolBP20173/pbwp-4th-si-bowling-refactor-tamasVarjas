f92d7358326da36d8af773743358ff4fc71bd93e
Tamás Varjas <tamas.varjas00@gmail.com>
2017-11-24 11:02:24 +0100

    Used autopep8 + removed commented-out lines

--- bowling.py
+++ bowling.py
@@ -7,21 +7,19 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[i + 2])
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
@@ -29,6 +27,7 @@ def score(game):
             in_first_half = True
             frame += 1
     return result
+
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
