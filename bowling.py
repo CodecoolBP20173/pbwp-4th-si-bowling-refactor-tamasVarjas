b837db43d18841cf60f714f9380f8c8b0d479aaf
Tamás Varjas <tamas.varjas00@gmail.com>
2017-11-24 15:50:47 +0100

    added newlines for better overview

--- bowling.py
+++ bowling.py
@@ -2,10 +2,12 @@ def score(game):
     NUMBER_OF_FRAMES = 10
     NUMBER_OF_PINS = 10
     OUTCOMES = {"strike": ("X", "x"), "spare": "/"}
+
     result = 0
     last = 0
     frame = 1
     in_first_half = True
+
     for i in range(len(game)):
         if game[i] == OUTCOMES["spare"]:
             result += NUMBER_OF_PINS - last
@@ -24,12 +26,14 @@ def score(game):
             in_first_half = True
         else:
             in_first_half = False
+
     return result
 
 
 def get_value(char):
     POSSIBLE_CHARS = {"numbers": ('1', '2', '3', '4', '5', '6', '7', '8', '9'),
                       "strike": ("X", "x"), "spare": "/", "miss": "-"}
+
     if char in POSSIBLE_CHARS["numbers"]:
         return int(char)
     elif char in POSSIBLE_CHARS["strike"] or char == POSSIBLE_CHARS["spare"]:
